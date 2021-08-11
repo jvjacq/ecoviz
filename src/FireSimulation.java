d4eb2ab1f0a3bfb8b3f14b0c2d87ab15c60912ba
owenfranke <owenfranke123@gmail.com>
2021-08-11 13:15:13 +0200

    Basic FireSimulation methods added

--- src/FireSimulation.java
+++ src/FireSimulation.java
@@ -1,3 +1,20 @@
 public class FireSimulation {
-    
+
+    private double speedMultiplier;
+    private String colour;
+
+    public FireSimulation() {
+        speedMultiplier = 0.0;
+        colour = "Red"; // Maybe have a secondary color to represent heat (i.e. orange, red or use RGB
+                        // range)
+    }
+
+    // Mutator Methods:
+    public void setSpeedMultiplier(double newMult) {
+        speedMultiplier = newMult;
+    }
+
+    public void setColour(String newColour) {
+        colour = newColour;
+    }
 }
