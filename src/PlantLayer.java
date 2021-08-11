fbf7923848756757eab6af6a45182963529f4234
owenfranke <owenfranke123@gmail.com>
2021-08-11 13:40:07 +0200

    Basic PlantLayer functions added

--- src/PlantLayer.java
+++ src/PlantLayer.java
@@ -1,3 +1,26 @@
 public class PlantLayer {
-    
+    private static int[][] idLocations;
+    private static int[][] burnt;
+
+    public PlantLayer() {
+        idLocations = null;
+        burnt = null;
+    }
+
+    // Methods:
+    public void removePlant() {
+
+    }
+
+    // Accessor Methods:
+    public PlantType getPlant() {
+        PlantType temp = new PlantType("", "", 0);
+        return temp;
+    }
+
+    // Mutator Methods:
+    public void setLayer() {
+
+    }
+
 }
