21795d2710de812f8487abd766b4051cbeca0de7
owenfranke <owenfranke123@gmail.com>
2021-08-11 13:29:21 +0200

    Basic PlantType methods added

--- src/PlantType.java
+++ src/PlantType.java
@@ -1,3 +1,24 @@
 public class PlantType {
-    
+    private String commonName;
+    private String latinName;
+    private int id;
+
+    public PlantType(String cName, String lName, int identity) {
+        commonName = cName;
+        latinName = lName;
+        id = identity;
+    }
+
+    // Accessor Methods:
+    public int getID() {
+        return id;
+    }
+
+    public String getDesc() {
+        String out = "Common Name: " + commonName + "/n";
+        out += "Common Name: " + commonName + "/n";
+        out += "Latin Name: " + latinName + "/n";
+
+        return out;
+    }
 }
