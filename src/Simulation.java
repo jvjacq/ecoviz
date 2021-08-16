368874e1599e4a7b5401d001574aeb4baaea26ce
owenfranke <owenfranke123@gmail.com>
2021-08-16 12:42:24 +0200

    readElevation() is working

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,12 +1,17 @@
+import java.io.FileNotFoundException;
+
 //Simulation.java - Main Class
 
 public class Simulation {
 
     private static Gui gui;
+    private static Terrain terrain;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         gui = new Gui();
         System.out.println("GUI is showing...");
+        terrain = new Terrain();
+        terrain.readElevation();
 
     }
 }
--- src/Terrain.java
+++ src/Terrain.java
@@ -1,1 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //Create 2D Greyscale enviro
+public class Terrain {
+    private double[][] elv;
+    private double gridSpacing = 0.0;
+    private double latitude = 0.0;
+    private int[][] canopy;
+    private int[][] undergrowth;
+    private int[][] species;
+
+    public Terrain(){
+
+    }
+
+    public void readElevation() throws FileNotFoundException{
+        String fileName = "src/data/S6000-6000-256.elv";
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        int dimX = Integer.parseInt(scanner.next());
+        int dimY = Integer.parseInt(scanner.next());
+        gridSpacing = Float.parseFloat(scanner.next());
+        latitude = Float.parseFloat(scanner.next());
+
+        elv = new double[dimY][dimX];
+
+        while (scanner.hasNext()){
+            for (int y=0; y<dimY;y++){
+                for (int x=0; x<dimX;x++){
+                    elv[y][x] = Double.parseDouble(scanner.next());
+                }
+            }
+        }
+        System.out.println("Elevation File has been read...");
+        scanner.close();
+    }
+    public void readCanopy(){
+        String file = "S6000-6000-256_canopy.pdb";
+    }
+    public void readUndergrowth(){
+        String file = "S6000-6000-256_undergrowth.pdb";
+    }
+    public void readSpecies(){
+        String file = "S6000-6000-256.spc.txt";
+    }
+}
