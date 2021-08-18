17c9193b5ac32419c4ca6d6f5a8d96d53a3ac930
JamesBurness <brnjam019@myuct.ac.za>
2021-08-18 16:35:55 +0200

    Completed (to current acceptance criteria) Wind.java functionality

--- src/Wind.java
+++ src/Wind.java
@@ -1,7 +1,16 @@
+/*
+* File: Wind.java
+* Author(s): BRNJAM019, FRNOWE001, VJRJAC003
+* Version 1.1
+* Created: +++++++++++ Owen insert date here +++++++
+* Last edited: 18/08/2021
+* Status: In progress
+*/
 public class Wind {
     private double direction;
     private double speed;
 
+    //Constructor
     public Wind() {
         direction = 0.0;
         speed = 0.0;
@@ -14,6 +23,15 @@ public class Wind {
 
     public void setSpeed(double newS) {
         speed = newS;
+    }
+
+    //Accessor methods:
+    public double getSpeed(){
+      return speed;
+    }
+
+    public double getDirection(){
+      return direction;
     }
 
 }
