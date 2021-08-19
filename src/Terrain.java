c035d05be75c77134ac56d671c5a715f73a7dcd9
owenfranke <owenfranke123@gmail.com>
2021-08-19 09:48:31 +0200

    Can now derive the top down greyscale img

--- src/Terrain.java
+++ src/Terrain.java
@@ -2,28 +2,46 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+
 
 //Create 2D Greyscale enviro
 public class Terrain {
     private double[][] elv;
+    private int dimX,dimY;
     private double gridSpacing = 0.0;
     private double latitude = 0.0;
     private double[][] canopy;
     private double[][] undergrowth;
     private ArrayList<PlantType> underPlants = new ArrayList<PlantType>();
-
     private ArrayList<PlantType> canopyPlants = new ArrayList<PlantType>();
+    private BufferedImage img;
+
     public Terrain(){
 
     }
+//Accessor Methods:
+    public int getDimX(){
+        return dimX;
+    }
+    public int getDimY(){
+        return dimY;
+    }
+    public BufferedImage getImg(){
+        return img;
+    }
 
+//========================================================================
+//      Read in the height values
+//========================================================================
     public void readElevation() throws FileNotFoundException{
         String fileName = "src/data/S6000-6000-256.elv";
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
 
-        int dimX = Integer.parseInt(scanner.next());
-        int dimY = Integer.parseInt(scanner.next());
+        dimX = Integer.parseInt(scanner.next());
+        dimY = Integer.parseInt(scanner.next());
         gridSpacing = Float.parseFloat(scanner.next());
         latitude = Float.parseFloat(scanner.next());
 
@@ -40,6 +58,36 @@ public class Terrain {
         scanner.close();
     }
 
+//========================================================================
+//      Create the greyscale top-down view
+//========================================================================
+    public void deriveImg(){
+        img = new BufferedImage(dimX,dimY,BufferedImage.TYPE_INT_ARGB);
+        double maxh = -10000.0f;
+        double minh = 10000.0f;
+
+        //determine the range of heights
+        for (int y = 0; y <dimY;y++){
+            for (int x = 0; x < dimX; x++){
+                double h = elv[y][x];
+                if (h>maxh){maxh = h;}
+                if (h<minh){minh = h;}
+            }
+        }
+
+        //find normalized height value
+        for (int y = 0; y <dimY;y++){
+            for (int x = 0; x < dimX; x++){
+                float val = (float) ((elv[y][x]-minh)/(maxh-minh));
+                Color col = new Color(val, val, val, 1.0f);
+                img.setRGB(x, y, col.getRGB());
+            }
+        }
+    }
+
+//========================================================================
+//      Read in Canopy
+//========================================================================
     public void readCanopy() throws FileNotFoundException{
         String fileName = "S6000-6000-256_canopy.pdb";
         File file = new File(fileName);
@@ -69,6 +117,9 @@ public class Terrain {
         scanner.close();
     }
 
+//========================================================================
+//      Read in Undergrowth
+//========================================================================
     public void readUndergrowth() throws FileNotFoundException{
         String fileName = "S6000-6000-256_undergrowth.pdb";
         File file = new File(fileName);
@@ -95,6 +146,10 @@ public class Terrain {
         }
         scanner.close();
     }
+
+//========================================================================
+//      Read in Species
+//========================================================================
     public void readSpecies(){
         String file = "S6000-6000-256.spc.txt";
     }
