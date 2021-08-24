286bec66889cd90511c3453a6c769341e1744e7e
Jacq Van Jaarsveld <vjrjac003@cs.uct.ac.za>
2021-08-24 18:05:17 +0000

    Added basic flood fill logic to FireSimulation.java

--- src/FireSimulation.java
+++ src/FireSimulation.java
@@ -17,4 +17,27 @@ public class FireSimulation {
     public void setColour(String newColour) {
         colour = newColour;
     }
+
+    public void floodFill(int xcoord, int ycoord, int previous, int locations[][])
+    {
+      if( xcoord < 0 || xcoord > terrainWidth || ycoord < 0 || ycoord > terrainHeight )
+      {
+        return;
+      }
+
+      for (int x = 0; x < terrainWidth; x++)
+      {
+        for (int y = 0; y < terrainHeight; y++)
+        {
+          if (locations[x][y] == 1)
+          {
+            burnt[xcoord][ycoord] = 1;
+            floodfill(xcoord, ycoord-1, 1);
+            floodfill(xcoord-1, ycoord, 1);
+            floodfill(xcoord, ycoord+1, 1);
+            floodfill(xcoord+1, ycoord, 1);
+          }
+        }
+      }
+    }
 }
