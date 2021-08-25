f5ddd752c211667c38bc77d5e4b395644d7331f5
JamesBurness <brnjam019@myuct.ac.za>
2021-08-25 10:20:11 +0200

    Added mph vs kph conversion methods to wind class

--- src/Wind.java
+++ src/Wind.java
@@ -1,19 +1,22 @@
 /*
 * File: Wind.java
 * Author(s): BRNJAM019, FRNOWE001, VJRJAC003
-* Version 1.1
+* Version 1.2
 * Created: +++++++++++ Owen insert date here +++++++
-* Last edited: 18/08/2021
+* Last edited: 25/08/2021
 * Status: In progress
 */
 public class Wind {
+    private final double CONVERT = 0.621371;
     private double direction;
     private double speed;
+    private boolean metric; //1 = kph, 0 = mph
 
     //Constructor
     public Wind() {
         direction = 0.0;
         speed = 0.0;
+        metric = true;
     }
 
     // Mutator methods:
@@ -27,11 +30,24 @@ public class Wind {
 
     //Accessor methods:
     public double getSpeed(){
-      return speed;
+      return (metric ? speedKPH() : speedMPH());
     }
 
     public double getDirection(){
       return direction;
+    }
+
+    public void toggleMetric(){
+      metric = !metric;
+    }
+
+    public double speedKPH(){
+      //Assuming speed from 0.0 - 100.0
+      return speed/2;
+    }
+
+    public double speedMPH(){
+      return (speed/2) * CONVERT;
     }
 
 }
