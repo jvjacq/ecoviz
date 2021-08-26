f2da7cf97307b1157b1d016e5d0c35e9069e8c15
Jacq Van Jaarsveld <vjrjac003@cs.uct.ac.za>
2021-08-26 19:00:23 +0000

    Fire algorithm abstract in FireSimulation.java

--- src/FireSimulation.java
+++ src/FireSimulation.java
@@ -2,11 +2,22 @@ public class FireSimulation {
 
     private double speedMultiplier;
     private String colour;
+    private int frameX, frameY;
 
     public FireSimulation() {
         speedMultiplier = 0.0;
         colour = "Red"; // Maybe have a secondary color to represent heat (i.e. orange, red or use RGB
                         // range)
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+
+    public FireSimulation(int frameWidth, int frameHeight) {
+        speedMultiplier = 0.0;
+        colour = "Red"; // Maybe have a secondary color to represent heat (i.e. orange, red or use RGB
+                        // range)
+        this.frameX = frameX;
+        this.frameY = frameY;
     }
 
     // Mutator Methods:
@@ -19,28 +30,101 @@ public class FireSimulation {
     }
 
 /*
+
+    ///// Fire simulation logic /////
+
+    if xcoord or ycoord not within terrain bounds:
+      return;
+
+    else:
+      if coordinates fall within a plant's radius:
+        for all plants in coordinate:
+          burnt[plantX][plantY] = plant radius * plant height
+
+          wind speed*
+
+          if (direction <= 45):
+            floodFill(xcoord+1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord, ycoord+1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+
+          if else (direction <= 90):
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord, ycoord+1, 1);
+
+          if else (direction <= 135):
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+
+          if else (direction <= 180):
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+
+          if else (direction <= 225):
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+
+          if else (direction <= 270):
+            floodFill(xcoord, ycoord+1, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord+1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+
+          if else (direction <= 315):
+            floodFill(xcoord, ycoord+1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+            floodFill(xcoord-1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+
+          if else (direction < 360):
+            floodFill(xcoord+1, ycoord, 1);
+            floodFill(xcoord, ycoord-1, 1);
+            floodFill(xcoord, ycoord+1, 1);
+            floodFill(xcoord+1, ycoord, 1);
+
+          burnt[previousX][previousY] -= 10; //reduce the previous plant's fuel/heat
+
+      else:
+        return;
+
+/*
     public void floodFill(int xcoord, int ycoord, int previous, int locations[][])
     {
-      if( xcoord < 0 || xcoord > terrainWidth || ycoord < 0 || ycoord > terrainHeight )
+      if( xcoord < 0 || xcoord > frameX || ycoord < 0 || ycoord > frameY )
       {
         return;
       }
 
-      for (int x = 0; x < terrainWidth; x++)
+      if( PlantLayer.getPlant(xcoord, ycoord) != null)
       {
-        for (int y = 0; y < terrainHeight; y++)
+        for (int x = 0; x < frameX; x++)
         {
-          if (locations[x][y] == 1)
+          for (int y = 0; y < frameY; y++)
           {
-            burnt[xcoord][ycoord] = 1;
-            floodfill(xcoord, ycoord-1, 1);
-            floodfill(xcoord-1, ycoord, 1);
-            floodfill(xcoord, ycoord+1, 1);
-            floodfill(xcoord+1, ycoord, 1);
+            if (locations[x][y] == 1)
+            {
+              burnt[xcoord][ycoord] = 1;
+              floodFill(xcoord, ycoord-1, 1);
+              floodFill(xcoord-1, ycoord, 1);
+              floodFill(xcoord, ycoord+1, 1);
+              floodFill(xcoord+1, ycoord, 1);
+            }
           }
         }
       }
+      else
+      {
+        return;
+      }
     }
 */
-
 }
