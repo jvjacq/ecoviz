32145c5a82db5f3885c8cf01a138088decee0d14
owenfranke <owenfranke123@gmail.com>
2021-08-26 22:20:44 +0200

    Gui Update: Zoom and Click

--- src/Gui.java
+++ src/Gui.java
@@ -22,6 +22,7 @@ import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.color.*;
 
@@ -34,6 +35,7 @@ public class Gui extends JPanel implements ActionListener{
     private JFileChooser fChooser;
     private JFrame frame;
     private JFrame loadIn;
+    private JMenuItem i3,i1;
     public Gui(int fX, int fY, Terrain land) {
         
 //======================================================================
@@ -75,7 +77,7 @@ loadIn = new JFrame("Initialising");
 //      North Panel:
 //======================================================================
         JPanel pnlNorth = new JPanel();
-
+        pnlNorth.setBackground(Color.GRAY);
         JLabel lblSearch = new JLabel("Search: ");
         JTextField search = new JTextField(20);
         JLabel lblFilter = new JLabel("Filter: ");
@@ -115,26 +117,28 @@ loadIn = new JFrame("Initialising");
             pnlEast.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
             pnlEast.setBorder(BorderFactory.createRaisedBevelBorder());
 
-            JTextArea plantDescription = new JTextArea();
+            JLabel heading = new JLabel("Plant Description");
+            Font f = heading.getFont();
+            heading.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+            JTextArea plantDescription = new JTextArea("Common Name:\nLatin Name:\nHeight:\nCanopy Radius:");
             pnlEast.add(plantDescription);
-        
+            pnlEast.setBackground(Color.lightGray);
+
             //Add Components
+            pnlEast.add(heading);
             pnlEast.add(plantDescription);
 
 //======================================================================
 //      South Panel:
 //======================================================================
         JPanel pnlSouth = new JPanel();
-            JLabel lblZoom = new JLabel("ZOOM: ");
-                JButton btnZoomIn = new JButton("+");
-                JButton btnZoomOut = new JButton("-");
-
+            JLabel lblZoom = new JLabel("Scroll to Zoom                       ");
+            lblZoom.setForeground(Color.white);
             JButton btnFire = new JButton("Simulate Fire");
-            
+            pnlSouth.setBackground(Color.DARK_GRAY);
+
             //Add Components
             pnlSouth.add(lblZoom);
-            pnlSouth.add(btnZoomIn);
-            pnlSouth.add(btnZoomOut);
             pnlSouth.add(btnFire);
 
 //====================================================================== 
@@ -143,9 +147,12 @@ loadIn = new JFrame("Initialising");
             JMenuBar mb = new JMenuBar();
 
             JMenu m1 = new JMenu("File");
-                JMenuItem i1 = new JMenuItem("Load Files");
+                i1 = new JMenuItem("Load Files");
+                i1.addActionListener(this);
                 JMenuItem i2 = new JMenuItem("Export as PNG");
-                JMenuItem i3 = new JMenuItem("Exit");
+                i3 = new JMenuItem("Exit");
+                i3.addActionListener(this);
+
                     m1.add(i1);
                     m1.add(i2);
                     m1.add(i3);
@@ -166,12 +173,12 @@ loadIn = new JFrame("Initialising");
         
         // Show
         frame.pack();
-        //frame.setVisible(true);
-        loadIn.setVisible(true);
+        frame.setVisible(true);
+        //loadIn.setVisible(true);
 
     }
     public void actionPerformed( ActionEvent e ){
-        if (e.getSource() == load){
+        if ((e.getSource() == load) | e.getSource() == i1){
             int returnVal = fChooser.showOpenDialog(Gui.this);
             if (returnVal == JFileChooser.APPROVE_OPTION){
                 File directory = fChooser.getSelectedFile();
@@ -182,5 +189,8 @@ loadIn = new JFrame("Initialising");
                 System.out.println("Opening the file");
             }else{System.out.println("Cancelled by the user");}
         }
+
+        if (e.getSource() == i3){System.exit(0);}
+
     }
 }
--- src/imgPanel.java
+++ src/imgPanel.java
@@ -11,12 +11,20 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.MouseInfo;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
+
+
 
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.geom.AffineTransform;
 
-public class imgPanel extends JPanel implements MouseWheelListener{
+public class imgPanel extends JPanel implements MouseWheelListener, MouseListener, MouseMotionListener{
 
 	private Terrain land;
 	private Graphics2D graph;
@@ -28,9 +36,17 @@ public class imgPanel extends JPanel implements MouseWheelListener{
 	private double prevZoomMultiplier = 1;
 	private boolean zoom;
 
+	private boolean zoomer,dragger,released;
+	private double xOffset,yOffset = 0;
+	private int xDiff,yDiff;
+	private Point startPoint;
+
 	public imgPanel(BufferedImage img){
 		this.img=img;
 		addMouseWheelListener(this);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+
 	}
 
 	@Override
@@ -41,15 +57,65 @@ public class imgPanel extends JPanel implements MouseWheelListener{
 
 		if (zoom) {
 			AffineTransform affine = new AffineTransform();
+
+			double xRelative = MouseInfo.getPointerInfo().getLocation().getX()-getLocationOnScreen().getX();
+			double yRelative = MouseInfo.getPointerInfo().getLocation().getY()-getLocationOnScreen().getY();
+
+			double divident = zoomMultiplier/prevZoomMultiplier;
+
+			xOffset = divident * xOffset + (1-divident)*xRelative;
+			yOffset = divident * yOffset + (1-divident)*yRelative;
+
+			affine.translate(xOffset,yOffset);
 			affine.scale(zoomMultiplier, zoomMultiplier);
 			prevZoomMultiplier = zoomMultiplier;
 			graphics2d.transform(affine);
 			zoom = false;
-		}
+		} 
 		
-		g.drawImage(img, 0, 0, null);
+		if (dragger){
+			AffineTransform affine = new AffineTransform();
+			affine.translate(xOffset+xDiff,yOffset+yDiff);
+			affine.scale(zoomMultiplier,zoomMultiplier);
+			graphics2d.transform(affine);
+
+			if (released){
+				xOffset += xDiff;
+				yOffset += yDiff;
+				dragger = false;
+			}
 
 		}
+		
+		graphics2d.drawImage(img, 0, 0, null);
+
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		Point cursor = e.getLocationOnScreen();
+		xDiff = cursor.x - startPoint.x;
+		yDiff = cursor.y - startPoint.y;
+
+		dragger = true;
+		repaint();
+
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		released= true;
+		repaint();
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		released = false;
+		startPoint= MouseInfo.getPointerInfo().getLocation();
+		
 	}
 
 	@Override
@@ -68,7 +134,39 @@ public class imgPanel extends JPanel implements MouseWheelListener{
 			repaint();
 		}
 
+
+
 	}
+
+	
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	
 
 
 }
