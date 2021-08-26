2a9d505cb484c7ba5dfae90c1cfc30e1f494ff94
JamesBurness <brnjam019@myuct.ac.za>
2021-08-26 23:09:47 +0200

    Added necessary method calls to simulation and reworked execution order

--- src/Simulation.java
+++ src/Simulation.java
@@ -14,10 +14,11 @@ import javax.swing.SwingUtilities;
 //Simulation.java - Main Class
 
 public class Simulation {
-
+    public static int frameX, frameY;
+    
     private static Gui gui;
     private static Terrain terrain;
-    //new
+    
     private static PlantLayer[] plantlayers;
     private static int numSpecies;
 
@@ -27,19 +28,23 @@ public class Simulation {
         terrain.readElevation(); 
         terrain.deriveImg();
 
-        int frameX=terrain.getDimX();   //Consider making theses static?
-        int frameY=terrain.getDimY();
-        SwingUtilities.invokeLater(() -> new Gui(frameX,frameY,terrain)); //in case we use threads
+        frameX=terrain.getDimX();   //Consider making theses static?
+        frameY=terrain.getDimY();
+        
         //Species objects
         PlantLayer.readSpecies();
         //plantlayer objects
         PlantLayer canopy = new PlantLayer("src/data/S2000-2000-512_canopy.pdb", frameX, frameY);
-        PlantLayer undergrowth = new PlantLayer("src/data/S2000-2000-512_canopy.pdb", frameX, frameY);
+        PlantLayer undergrowth = new PlantLayer("src/data/S2000-2000-512_undergrowth.pdb", frameX, frameY);
         
         plantlayers = new PlantLayer[2];
         plantlayers[0] = undergrowth;
         plantlayers[1] = canopy;
 
+        canopy.deriveImg();
+        undergrowth.deriveImg();
+
+        SwingUtilities.invokeLater(() -> new Gui(frameX,frameY,terrain,canopy,undergrowth)); //in case we use threads
         //gui = new Gui(terrain);
         System.out.println("GUI is showing...");
     }
