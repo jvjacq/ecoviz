451a51b318a94f7b0f99fa5e69fe75d0a17292ac
JamesBurness <brnjam019@myuct.ac.za>
2021-08-26 12:14:34 +0200

    Refactored the constructor/create method

--- src/Species.java
+++ src/Species.java
@@ -1,3 +1,13 @@
+/*
+* Previously: PlantLayer.java
+* File: Species.java
+* Author(s): BRNJAM019, FRNOWE001, VJRJAC003
+* Version 1.3
+* Created: ++++++++Owen insert date++++++++
+* Last edited: 26/08/2021
+* Status: In progress
+*/
+
 public class Species {
     private String commonName;
     private String latinName;
@@ -8,8 +18,17 @@ public class Species {
     private int numPlants;
     private Plant[] list;
 
-
-    public Species(int id, String cname, String lname) {
+    public Species(int id, float minH, float maxH, float rad, int numP) {
+        
+        this.id = id;
+        minHeight = minH;
+        maxHeight = maxH;
+        avgRadius = rad;
+        numPlants = numP;
+        list = new Plant[numP];
+        
+    } 
+    /*public Species(int id, String cname, String lname) {
         
         this.id = id;
         commonName = cname;
@@ -19,17 +38,21 @@ public class Species {
         avgRadius = 0.0f;
         numPlants = 0;
         
-    }
+    } 
+    //New idea im working on
 
     public void create(float minH, float maxH, float rad, int numP){
         minHeight = minH;
         maxHeight = maxH;
         avgRadius = rad;
         numPlants = numP;
-    }
+    }*/
 
     
     //Mutator Methods:
+    public void addPlant(Plant plant){
+        list[plant.getID()] = plant;
+    }
 
     // Accessor Methods:
     public int getID() {
