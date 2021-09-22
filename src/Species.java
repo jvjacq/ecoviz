ac9228aaabc9ee7f1d46acaea791d1e53ddcfe2a
JamesBurness <brnjam019@myuct.ac.za>
2021-09-22 13:32:00 +0200

    Fully implemented new constructor. Old constructor still needing to be removed

--- src/Species.java
+++ src/Species.java
@@ -12,8 +12,8 @@ import java.awt.Color;
 
 public class Species {
 
-    private static String[][] SPECIES;
-    private static int[] COLOURS;
+    //private static String[][] SPECIES;
+    //private static int[] COLOURS;
     private String commonName;
     private String latinName;
     private int speciesid;
@@ -24,21 +24,29 @@ public class Species {
     private Plant[] canopy;
     private Plant[] undergrowth;
     private Color colour;
+    private boolean filter;
 
 
-    public Species(String common, String latin){
+    public Species(int id, String common, String latin, Color colour){
+        this.speciesid = id;
         this.commonName = common;
         this.latinName = latin;
+        this.colour = colour;
+        this.minHeight = -1;
+        this.maxHeight = -1;
+        this.avgRatio = 0;
+        this.numPlants = 0;
+        this.filter = true;
     }
-    
+
     public Species(int id, float minH, float maxH, float avg, int num){
         this.speciesid = id;
         this.minHeight = minH;
         this.maxHeight = maxH;
         this.avgRatio = avg;
         this.numPlants = num;
-        this.commonName = Species.SPECIES[id][0];
-        this.latinName = Species.SPECIES[id][1];
+        //this.commonName = Species.SPECIES[id][0];
+        //this.latinName = Species.SPECIES[id][1];
     }
 
     //Mutator Methods:
@@ -79,17 +87,21 @@ public class Species {
         this.undergrowth = list;
     }
 
-    public void setColour(int rgb){
-        this.colour = new Color(rgb, true);
+    public void setColour(Color col){
+        this.colour = col;
     }
 
-    public static void setSpeciesList(String[][] list){
+    public void setFilter(boolean b){
+        this.filter = b;
+    }
+
+    /*public static void setSpeciesList(String[][] list){
         Species.SPECIES = list;
     }
 
     public static void setColourList(int[] list){
         Species.COLOURS = list;
-    }
+    }*/
 
     // Accessor Methods:
 
@@ -129,17 +141,21 @@ public class Species {
         return this.undergrowth;
     }
 
-    public int getColour(){
-        return this.colour.getRGB();
+    public Color getColour(){
+        return this.colour;
     }
 
-    public static String[][] getSPECIES(){
+    public boolean getFilter(){
+        return this.filter;
+    }
+
+    /*public static String[][] getSPECIES(){
         return Species.SPECIES;
     }
 
     public static int[] getCOLOURS(){
         return Species.COLOURS;
-    }
+    }*/
 
     @Override
     public String toString() {
