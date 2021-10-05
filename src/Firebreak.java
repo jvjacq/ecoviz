b2a290e875f96b4ee8055c88a8b04c2bf73d5832
JamesBurness <brnjam019@myuct.ac.za>
2021-10-06 01:11:28 +0200

    Added new getters for 2 new arraylists

--- src/Firebreak.java
+++ src/Firebreak.java
@@ -4,12 +4,14 @@ public class Firebreak{
     private static ArrayList<Firebreak> breaklist;
     private ArrayList<int[]> region;
     private int minX, maxX, minY, maxY;
+    private ArrayList<Boolean> layers;
     private ArrayList<int[]> ids;
 
     public Firebreak(){
         if(breaklist == null) breaklist = new ArrayList<Firebreak>();
         region = new ArrayList<int[]>();
         ids = new ArrayList<int[]>();
+        layers = new ArrayList<Boolean>();
         maxX = -1; minX = -1; minY = -1; maxY = -1;
     }
 
@@ -17,12 +19,20 @@ public class Firebreak{
         return this.ids;
     }
 
+    public ArrayList<Boolean> getLayers(){
+        return this.layers;
+    }
+
+    public static ArrayList<Firebreak> getBreakList(){
+        return Firebreak.breaklist;
+    }
+
     public void addToRegion(int x, int y){
         int[] pair = new int[2];
         if(x > maxX) maxX = x;
-        if(x < minX) minX = x;
+        if(x < minX || minX == -1) minX = x;
         if(y > maxY) maxY = y;
-        if(y < minY) minY = y;
+        if(y < minY || minY == -1) minY = y;
         pair[0] = x;
         pair[1] = y;
         if(!region.contains(pair)){
@@ -35,6 +45,10 @@ public class Firebreak{
         breaklist.add(fb);
     }
 
+    public static void removeLatest(){
+        breaklist.remove(breaklist.size()-1);
+    }
+
     public boolean inFirebreak(Plant p){
         //System.out.println(region.size());
         if((p.getX() > maxX) || (p.getX() < minX) || (p.getY() > maxY) || (p.getY() < minY)) return false; 
@@ -43,6 +57,7 @@ public class Firebreak{
             if((p.getX() == region.get(i)[0]) && (p.getY() == region.get(i)[1])){
                 int[] pair = {p.getSpeciesID(), p.getID()};
                 ids.add(pair);
+                layers.add(p.getLayer());
                 return true;
             }
         }
