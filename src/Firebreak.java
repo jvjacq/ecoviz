26363c1edfa9c6c5e61a134bc41e0b1dcdc08aad
JamesBurness <brnjam019@myuct.ac.za>
2021-10-05 19:27:54 +0200

    Reworked firebreak class

--- src/Firebreak.java
+++ src/Firebreak.java
@@ -1,27 +1,44 @@
 import java.util.ArrayList;
 
-public class Firebreak {
+public class Firebreak{
+    private static ArrayList<Firebreak> breaklist;
+    private ArrayList<int[]> region;
+    private int minX, maxX, minY, maxY;
+    private int[][] ids;
 
-  private static ArrayList<Firebreak> breaklist;
-  private ArrayList<int[]> region;
-  public Firebreak(){
-    region = new ArrayList<int[]>();
-  }
-
-  public void addToRegion(int x, int y, int radius){
-    int[] circle  = {x,y,radius};
-    region.add(circle);
-  }
-
-  public static void addCompleted(Firebreak fb){
-    breaklist.add(fb);
-  }
-
-  public boolean inFirebreak(Plant p){
-    for(int[] i: region){
-      if(Math.pow((p.getX() - i[0]),2) + Math.pow((p.getY() - i[1]),2) <= Math.pow(i[2],2)) return true;
+    public Firebreak(){
+        if(breaklist == null) breaklist = new ArrayList<Firebreak>(1);
+        region = new ArrayList<int[]>(1);
+        maxX = -1; minX = -1; minY = -1; maxY = -1;
     }
-    return false;
-  }
 
+    public void addToRegion(int x, int y){
+        int[] pair = new int[2];
+        if(x > maxX) maxX = x;
+        if(x < minX) minX = x;
+        if(y > maxY) maxY = y;
+        if(y < minY) minY = y;
+        pair[0] = x;
+        pair[1] = y;
+        if(!region.contains(pair)){
+            region.add(pair);
+            //System.out.println(region.get(region.size() -1));
+        }
+    }
+
+    public static void addCompleted(Firebreak fb){
+        breaklist.add(fb);
+    }
+
+    public boolean inFirebreak(Plant p){
+        //System.out.println(region.size());
+        if((p.getX() > maxX) || (p.getX() < minX) || (p.getY() > maxY) || (p.getY() < minY)) return false; 
+        for(int i = 0; i < region.size(); ++i){
+            //System.out.println(region.get(i)[0]);
+            if((p.getX() == region.get(i)[0]) && (p.getY() == region.get(i)[1])){
+                return true;
+            }
+        }
+        return false;
+    }
 }
