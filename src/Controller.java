41c4bc1df46169e86a8b4ce34a0415738ae951e9
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 19:52:21 +0200

    Button enabling bug fix

--- src/Controller.java
+++ src/Controller.java
@@ -423,6 +423,7 @@ public class Controller implements MouseWheelListener, MouseListener, MouseMotio
         gui.getTabPane().setEnabled(false);
         gui.getFireBtn().setVisible(false);
         gui.getChkFirebreak().setVisible(true);
+        gui.getChkFirebreak().setEnabled(true);
         gui.getUndoBtn().setVisible(true);
         gui.getPauseBtn().setVisible(true);
         gui.getBackBtn().setVisible(true);
@@ -506,6 +507,7 @@ public class Controller implements MouseWheelListener, MouseListener, MouseMotio
 
     public void renderFireSim() { // Single use
         frames=0;
+        gui.getChkFirebreak().setEnabled(false);
         gui.getCloseRender().setEnabled(true);
         System.out.println("Running the Fire Simulation");
         gui.getPauseBtn().setEnabled(true);
