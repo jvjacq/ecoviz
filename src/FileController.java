73f7f82b68a4b3bca80f8273a23a53f60778e172
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 12:05:09 +0200

    Redid previous zpos touch

--- src/FileController.java
+++ src/FileController.java
@@ -237,7 +237,7 @@ public class FileController implements Runnable{
                 xpos = (int)Math.round(Float.parseFloat(filein.next())/0.9144*scale);
                 ypos = (int)Math.round(Float.parseFloat(filein.next())/0.9144*scale);
                 zpos = Math.round(Float.parseFloat(filein.next()));  //Intentionally unused
-                zpos = 7*zpos; //irrelevant, just my favourite number
+                zpos = 1*zpos; //irrelevant, just removed VSCode warning
                 height = Float.parseFloat(filein.next());
                 canopy = Float.parseFloat(filein.next())*scale;
 
